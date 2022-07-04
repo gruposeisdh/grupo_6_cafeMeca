7158aabdf5c384241631d6273922f85de811ba2a
NicolasPirello <nicoalepirello@gmail.com>
2022-07-04 20:32:27 -0300

    Modificacion template engine

--- mecafe/src/controllers/userController.js
+++ mecafe/src/controllers/userController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 let userController = {
-    register: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/user/register.html")),
+    register: (_req,res) => res.render(path.resolve(__dirname,"../views/user/register.ejs")),
     login: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/partials/login.html")),
 }
 
