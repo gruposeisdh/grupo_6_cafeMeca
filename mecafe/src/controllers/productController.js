79ce0d61a022250e34a22627e75e97afb3dc4956
NicolasPirello <nicoalepirello@gmail.com>
2022-07-04 22:02:13 -0300

    Nueva actualizacion detalles de ejs

--- mecafe/src/controllers/productController.js
+++ mecafe/src/controllers/productController.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 let productController = {
-    index: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/product/list.html")),
+    index: (_req,res) => res.render(path.resolve(__dirname,"../views/product/list.ejs")),
     create: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/product/create.html")),
     detail: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/product/product.html"))
 }
