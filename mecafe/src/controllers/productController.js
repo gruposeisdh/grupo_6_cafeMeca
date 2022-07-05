3414ef2864848e89f1156145a655de23402cba4e
jflores <johflores1510@gmail.com>
2022-07-04 21:56:32 -0400

    wip script js header en detailProduct

--- mecafe/src/controllers/productController.js
+++ mecafe/src/controllers/productController.js
@@ -3,7 +3,7 @@ const path = require('path');
 let productController = {
     index: (_req,res) => res.render(path.resolve(__dirname,"../views/product/list.ejs")),
     create: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/product/create.html")),
-    detail: (_req,res) => res.sendFile(path.resolve(__dirname,"../views/product/product.html"))
+    detail: (_req,res) => res.render(path.resolve(__dirname,"../views/product/product.ejs"))
 }
 
 module.exports = productController;
