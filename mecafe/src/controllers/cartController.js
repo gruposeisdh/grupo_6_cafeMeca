5b8b44f6fa20d437ad7d6eee99a61c5e088cdc4e
jflores <johflores1510@gmail.com>
2022-07-20 22:28:41 -0400

    cart wip

--- mecafe/src/controllers/cartController.js
+++ mecafe/src/controllers/cartController.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 let cartController = {
-    index: (_req,res) => res.render(path.resolve(__dirname,"../views/cart.ejs"))
+    index: (_req,res) => res.render(path.resolve(__dirname,"../views/cart.ejs")),
+    indexN: (_req,res) => res.render(path.resolve(__dirname,"../views/cartNew.ejs"))
 }
 
 module.exports = cartController;
--- mecafe/src/routes/cart.js
+++ mecafe/src/routes/cart.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const cartController = require('../controllers/cartController.js')
 
 router.get('/',cartController.index);
+router.get('/new',cartController.indexN);
 
 module.exports = router;
