a633e373afe42c3513ca2f81d69dbf3789ebaeed
jflores <johflores1510@gmail.com>
2022-08-10 22:19:42 -0400

    fix

--- mecafe/database/config/config.js
+++ mecafe/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "meCafe",
+    "database": "me_cafe",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": false
