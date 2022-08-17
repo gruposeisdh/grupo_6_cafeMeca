82e02bbff1948bb84a2f1c0d93f053d8a9df5034
NicolasPirello <nicoalepirello@gmail.com>
2022-08-17 02:45:31 -0300

    Modelos y relaciones de Nico. Tambien subi los PDF de los sprint para que los tengamos

---
+++ mecafe/database/models/Product.js
@@ -0,0 +1,67 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = "Product"
+
+    let cols = {
+
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.STRING,
+            allowNull: false,
+        },
+        rating: {
+            type: dataTypes.INTEGER
+        },
+        description: {
+            type: dataTypes.STRING
+        },
+        brand_id: {
+            type: dataTypes.INTEGER,
+            allowNull: false,
+        },
+
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: false
+    }
+
+    // Declaracion de Product con sus definiciones
+
+    const Product = sequilize.define(alias, cols, config)
+
+    // Asociaciones
+
+    Product.associate = (models) => {
+
+        Product.belongsToMany( models.TypeGrinding, { // Tabla con la que se va a RELACIONAR
+            as: "typegrindings",
+            through: "products_type_grindings", // Tabla INTERMEDIA
+            foreignKey: "product_id",
+            otherKey: "type_grinding_id",
+            timestamps: false
+        })
+
+        // hasmany
+
+        Product.belongsTo (models.Brand, {
+            as: "brands",
+            foreignKey: "brand_id"
+        })
+
+        Product.hasMany (models.ImageProduct, {
+            as: "imagesproducts",
+            foreignKey: "product_id"
+        })
+
+    }
+
+    return Product
+
+}
---
+++ mecafe/database/models/TypeGrinding.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, dataTypes) => {
+
+    let alias = "TypeGrinding"
+
+    let cols = {
+
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: dataTypes.STRING,
+            allowNull: false // Esto no deberia poder ser nulo en la Base de datos.
+        }
+
+    }
+
+    let config = {
+        tableName: "",
+        timestamps: false
+    }
+
+    const TypeGrinding = sequelize.define (alias, cols, config)
+
+    // Asociaciones
+
+    TypeGrinding.associate = (models) => {
+
+        TypeGrinding.belongsToMany(models.Product, { // Tabla con la que se va a RELACIONAR
+            as: "products",
+            through: "products_type_grindings", // Tabla INTERMEDIA
+            foreignKey: "type_grinding_id",
+            otherKey: "product_id",
+            timestamps: false
+        })
+
+    }
+
+    return TypeGrinding
+    
+}
