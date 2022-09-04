134d888d860bc35519ac2d22ea11859c0749757a
jflores <johflores1510@gmail.com>
2022-09-04 15:45:40 -0400

    detalle carrito rest

--- mecafe/src/controllers/cartController.js
+++ mecafe/src/controllers/cartController.js
@@ -5,14 +5,35 @@ const { Cart } = require("../../database/models");
 let cartController = {
     index: (_req,res) => {
         //let id =  req.session.user.id;
-        let id= 1;
-       
-        Cart.findOne(
-            {include: [{association : "users"}]},
-            {where: {'user_id' : id} }).then((cart) => { 
-            res.send(cart);
-        })
+        let userId= 1;
 
+        db.DetailCart.findAll(          
+            {include: [
+                {
+                    model: db.Cart, as: "carts", where: {'user_id': userId}, attributes: []
+                },
+                {
+                    model: db.ProductGrame, as: "products_grames", 
+                    attributes: ['grames','price'], 
+                    include: [{
+                        model: db.Product, as: "products", attributes:['name'] , 
+                        include: [{
+                            model: db.Brand, as: "brands"
+                        }]
+                    }]
+                },
+                {
+                    model: db.ProductTypeGrinding, as: "products_type_grindings", 
+                    attributes:['type_grinding_id'], 
+                    include : [{
+                        model: db.TypeGrinding, as: "type_grindings" , attributes: ['name']}
+                    ]
+                },
+            ]}        
+        )
+        .then((detailsCart) => { 
+            res.send(detailsCart);
+        })
         //res.render(path.resolve(__dirname,"../views/cart.ejs"))
     }
 }
