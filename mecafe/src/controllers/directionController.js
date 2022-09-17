2b35bf27ef04999c768a6d8c31e5e1ea78268221
jflores <johflores1510@gmail.com>
2022-09-16 21:02:33 -0300

    mis direcciones

--- mecafe/src/controllers/directionController.js
+++ mecafe/src/controllers/directionController.js
@@ -8,8 +8,15 @@ const { Op } = require("sequelize");
 
 let directionController = {
     index: (_req,res) => {
-       
-        res.render(path.resolve(__dirname, "../views/user/direction/list.ejs"))
+        let id= 1;    
+        //let id=  req.session.user.id;
+        db.Direction.findAll({
+            where:{user_id:id}, 
+            order: [['default', 'desc']]})
+        .then((directions) => {
+            //res.send(directions);
+            res.render(path.resolve(__dirname, "../views/user/direction/list.ejs"),{directions:directions});
+        })
     },
 }
 
