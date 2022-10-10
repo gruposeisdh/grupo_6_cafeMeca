38a24ea2de3fc6276add5755dafcfaa20b3512a7
NicolasPirello <nicoalepirello@gmail.com>
2022-10-10 07:10:52 -0300

    Seguimos avanzando mas y mas

--- mecafe/public/js/appHeader.js
+++ mecafe/public/js/appHeader.js
@@ -21,22 +21,22 @@ const navbarPerfil = document.getElementById("navbarPerfil")
 
 // Variables de Divs que queremos desplegar (Combinacion de IF con clase .nav-link-desplagable)
 
-const DivQuienesSomos = document.querySelector("#quienesSomos .nav-link-desplagable")
-const DivAdmin = document.querySelector("#navbarAdmin .nav-link-desplagable")
-const DivPerfil = document.querySelector("#navbarPerfil .nav-link-desplagable")
+const divQuienesSomos = document.querySelector("#quienesSomos .nav-link-desplagable")
+const divAdmin = document.querySelector("#navbarAdmin .nav-link-desplagable")
+const divPerfil = document.querySelector("#navbarPerfil .nav-link-desplagable")
 
 // Funcion para Reutilizar
 
-function desplegar (selectorId, DivDesplegar) {
+function desplegar (selectorId, divDesplegar) {
     selectorId.addEventListener("click", () => {
-        DivDesplegar.classList.toggle("nav-link-aparecer")
+        divDesplegar.classList.toggle("nav-link-aparecer")
         selectorId.classList.toggle("link-hover")
     })
 }
 
 // Llamado a Funciones
 
-desplegar(quienesSomos, DivQuienesSomos)
-desplegar(navbarAdmin, DivAdmin)
-desplegar(navbarPerfil, DivPerfil)
+desplegar(quienesSomos, divQuienesSomos)
+desplegar(navbarAdmin, divAdmin)
+desplegar(navbarPerfil, divPerfil)
 
