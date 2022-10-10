ee2d05ada4ec79050bd5b5ec0bd9c1e286f87832
NicolasPirello <nicoalepirello@gmail.com>
2022-10-10 04:53:26 -0300

    Nuevos cambios NavBar

--- mecafe/public/js/appHeader.js
+++ mecafe/public/js/appHeader.js
@@ -5,16 +5,38 @@
 const navToggle = document.querySelector("#navToggle")
 const nav = document.querySelector("#nav-links")
 
-const itemSelector = document.querySelector(".selector-Nav")
-const mostrarDiv = document.querySelector(".nav-link-desplagable")
-
 // add event listener
 
 navToggle.addEventListener("click", () => {
     nav.classList.toggle('nav-open')
 })
 
-itemSelector.addEventListener("click", () => {
-    mostrarDiv.classList.toggle("nav-link-aparecer")
-    itemSelector.classList.toggle("link-hover")
-})
+// NavBar Desplegables
+
+// Variables de cada uno de los items que quiero desplegar
+
+const quienesSomos = document.getElementById("quienesSomos")
+const navbarAdmin = document.getElementById("navbarAdmin")
+const navbarPerfil = document.getElementById("navbarPerfil")
+
+// Variables de Divs que queremos desplegar (Combinacion de IF con clase .nav-link-desplagable)
+
+const DivQuienesSomos = document.querySelector("#quienesSomos .nav-link-desplagable")
+const DivAdmin = document.querySelector("#navbarAdmin .nav-link-desplagable")
+const DivPerfil = document.querySelector("#navbarPerfil .nav-link-desplagable")
+
+// Funcion para Reutilizar
+
+function desplegar (selectorId, DivDesplegar) {
+    selectorId.addEventListener("click", () => {
+        DivDesplegar.classList.toggle("nav-link-aparecer")
+        selectorId.classList.toggle("link-hover")
+    })
+}
+
+// Llamado a Funciones
+
+desplegar(quienesSomos, DivQuienesSomos)
+desplegar(navbarAdmin, DivAdmin)
+desplegar(navbarPerfil, DivPerfil)
+
