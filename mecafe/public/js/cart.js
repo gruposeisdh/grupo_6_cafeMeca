ce81dbd17ca5646bf82e02a89bfeb42b2c5ac68b
NicolasPirello <nicoalepirello@gmail.com>
2022-10-20 20:51:21 -0300

    Vista Checkout sin Logica

--- mecafe/public/js/cart.js
+++ mecafe/public/js/cart.js
@@ -9,3 +9,21 @@ function moreValue(id){
     let input = document.querySelector('#item_' + id + ' input[type="number"]');
     input.value = parseInt(input.value) + 1;
 }
+
+/* Pagina Checkout  */
+
+let tableDetails = document.querySelector(".checkout__table")
+let buttonTableHidden = document.getElementById("checkout__tableHidden")
+
+buttonTableHidden.addEventListener ("click", () => {
+    
+    tableDetails.classList.toggle("checkout__table")
+
+})
+
+let dataDirectionBtn = document.getElementById("dataDirection-btn")
+let directionHidden = document.getElementById("dataDirectionContainer")
+
+dataDirectionBtn.addEventListener ("click", () => {
+    directionHidden.classList.toggle("dataDirectionHidden")
+})
