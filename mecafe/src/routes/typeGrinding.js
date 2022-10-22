35ce77b21c5d4115b456c6d573b7ac628fc770c2
jszuluaga <johanas_zuluaga@hotmail.com>
2022-10-22 15:47:28 -0500

    ajustando validaciones

--- mecafe/src/routes/typeGrinding.js
+++ mecafe/src/routes/typeGrinding.js
@@ -5,13 +5,30 @@ const path = require ('path');
 const multer = require('multer');
 const typeGrindingController = require('../controllers/typeGrindingController.js');
 const authMiddlewares = require('../middlewares/authMiddlewares');
+const db = require("../../database/models");
 
 const validateUpdateGrinding = [ 
-    check("nameTypeGrinding").notEmpty().withMessage("Debes ingresar un nombre"),
+    check("nameTypeGrinding").notEmpty().withMessage("Debes ingresar un nombre").bail()
+    .custom((value) => {
+        return db.TypeGrinding.findOne({ where: { name: value} }).then((grinding) => {
+          if (!grinding) {
+            return true;
+          }
+          return Promise.reject("Este tipo de molienda ya existe");
+        });
+      }),
 ]
 
 const validateCreateGrinding = [ 
-    check("nameGrinding").notEmpty().withMessage("Debes ingresar un nombre"),
+    check("nameGrinding").notEmpty().withMessage("Debes ingresar un nombre").bail()
+    .custom((value) => {
+        return db.TypeGrinding.findOne({ where: { name: value} }).then((grinding) => {
+          if (!grinding) {
+            return true;
+          }
+          return Promise.reject("Este tipo de molienda ya existe");
+        });
+      }),
 ]
 
 router.get(
